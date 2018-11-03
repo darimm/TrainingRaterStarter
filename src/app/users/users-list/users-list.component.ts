df841da3a9f260024bdf47844c93f53f0e037014
Scott Stanley <darimm@gmail.com>
2018-11-02 22:53:37 -0400

    Added the ability to add and delete users Created an event binding for changed data to check if the most recently added user has been updated yet before allowing addition of another user.

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -8,10 +8,36 @@ import { UsersService } from '../users.service';
 })
 export class UsersListComponent implements OnInit {
   users = [];
+  canAddUser = false;
   constructor(private usersService: UsersService) { }
 
   ngOnInit() {
     this.users = this.usersService.getUsers();
   }
 
+  addUser(): void {
+    this.users.push(new Object(
+      {userName: 'username', firstName: 'First', lastName: 'Last', password: 'Password'}
+    ));
+    this.canAddUser = !this.canAddUser;
+  }
+
+  formChanged(event) {
+    console.log(event);
+    console.log(this.users[this.users.length - 1].userName);
+    if (
+      (this.users[this.users.length - 1].userName !== 'username')
+    && (this.users[this.users.length - 1].firstName !== 'First')
+    && (this.users[this.users.length - 1].lastName !== 'Last')
+    && (this.users[this.users.length - 1].password !== 'Password')
+    && (this.canAddUser = true)
+    ) {
+      console.log('Got here.');
+      this.canAddUser = !this.canAddUser;
+    }
+  }
+
+  deleteUser(index: number) {
+    this.users.splice(index, 1);
+  }
 }
